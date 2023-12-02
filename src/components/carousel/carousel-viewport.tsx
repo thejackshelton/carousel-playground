27fc1f148cb816c2f543766b1ef5505ff600cae2
thejackshelton <me@jackshelton.com>
2023-12-02 14:36:17 -0600

    first commit

---
+++ src/components/carousel/carousel-viewport.tsx
@@ -0,0 +1,53 @@
+import {
+  component$,
+  type QwikIntrinsicElements,
+  Slot,
+  useContext,
+  $,
+} from "@builder.io/qwik";
+import CarouselContextId from "./carousel-context-id";
+
+type CarouselViewportProps = QwikIntrinsicElements["div"];
+
+export const CarouselView = component$((props: CarouselViewportProps) => {
+  const context = useContext(CarouselContextId);
+
+  return (
+    <div
+      onPointerDown$={(e) => {
+        context.initialX.value = e.clientX;
+        if (context.containerRef.value) {
+          const style = window.getComputedStyle(context.containerRef.value);
+          const matrix = new DOMMatrix(style.transform);
+          context.initialTransformX.value = matrix.m41;
+
+          console.log("Pointer down");
+          context.isDraggingSig.value = true;
+        }
+
+        window.addEventListener(
+          "pointermove",
+          // @ts-ignore
+          $((e) => {
+            if (context.isDraggingSig.value) {
+              if (!context.containerRef.value) {
+                return;
+              }
+
+              const deltaX = e.clientX - context.initialX.value;
+
+              context.slideOffsetSig.value =
+                context.initialTransformX.value + deltaX;
+            }
+          })
+        );
+      }}
+      ref={context.viewportRef}
+      style={{ overflow: "hidden", position: "relative" }}
+      onTransitionEnd$={() => (context.transitionDurationSig.value = 0)}
+      {...props}
+    >
+      <Slot />
+    </div>
+  );
+});
---
+++ src/components/carousel/pagination.tsx
@@ -0,0 +1,35 @@
+import { type QRL,type QwikIntrinsicElements, component$, useContext } from '@builder.io/qwik';
+import CarouselContextId from './carousel-context-id';
+import { range } from '../../utils/range';
+import { type JSX } from '@builder.io/qwik/jsx-runtime';
+
+/* Why we use the range util: 
+https://www.joshwcomeau.com/snippets/javascript/range/
+*/
+
+type CarouselPaginationProps = QwikIntrinsicElements['span'] & {
+  renderBullet$?: QRL<(n: number) => JSX.Element>;
+};
+
+export const CarouselPagination = component$(({ ...props }: CarouselPaginationProps) => {
+  const context = useContext(CarouselContextId);
+
+  return (
+    <>
+      {range(1, context.numSlidesSig.value + 1).map((num: number) => (
+        <>
+          {/* {renderBullet$ && symbolsArr.length >= num && renderBullet$(num - 1)} */}
+
+          <span
+            key={num}
+            {...props}
+            aria-current={context.currentIndexSig.value === num}
+            data-current-slide={context.currentIndexSig.value === num}
+          >
+            {num}
+          </span>
+        </>
+      ))}
+    </>
+  );
+});
